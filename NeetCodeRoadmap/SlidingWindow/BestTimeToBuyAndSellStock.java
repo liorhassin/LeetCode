7835f2ee772508a9c188d3b585d7bb123b979268
Lior Hassin <86710692+liorhassin@users.noreply.github.com>
2023-06-13 17:58:20 +0300

    initialize other packages

---
+++ NeetCodeRoadmap/SlidingWindow/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,13 @@
+package NeetCodeRoadmap.SlidingWindow;
+
+public class BestTimeToBuyAndSellStock {
+
+    public static int bestTimeToBuyAndSellStockGoodSolution(int[] prices){
+        if(prices.length == 0) return 0;
+        return 1; //TODO - WORK IN PROGRESS
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
